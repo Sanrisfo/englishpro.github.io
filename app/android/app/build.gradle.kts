5a320adac1908919313c5532a38354e7c2a74e82
angelogp27 <angelo27goitia@gmail.com>
2025-10-30 00:40:52 -0500

    correccion plugins

--- app/android/app/build.gradle.kts
+++ app/android/app/build.gradle.kts
@@ -3,7 +3,6 @@ plugins {
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
-    id("com.google.gms.google-services")
 }
 
 android {
@@ -22,7 +21,7 @@ android {
 
     defaultConfig {
         applicationId = "com.englishpro.englishpro_app"
-        minSdk = flutter.minSdkVersion  // Required for Firebase
+        minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
@@ -31,13 +30,11 @@ android {
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
         }
     }
 }
 
 flutter {
-    source = "../.."
+    source="../.."
 }
