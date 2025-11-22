4ba09472e6856ff8b019e81896e87cc82a5578f3
Santiago de Jesus Rodriguez Perez <santiago.rodriguezp@unmsm.edu.pe>
2025-11-22 16:18:09 -0500

    Frontend update 8 -Frontend pantalla de teacher questions -Frontend lista de actividades del estudiante -Cambios menores (iconos, colores, textos)

--- app/lib/screens/teacher_activity_types_screen.dart
+++ app/lib/screens/teacher_activity_types_screen.dart
@@ -473,7 +473,7 @@ class _TeacherActivityTypesScreenState
             ),
 
             IconButton(
-              icon: Icon(Icons.edit_rounded,
+              icon: Icon(Icons.mode_edit_outlined,
                   color: Color(0xFF23408E)),
               onPressed: () => _renameTypeDialog(t),
             ),
@@ -483,7 +483,7 @@ class _TeacherActivityTypesScreenState
               onPressed: () => _editAllowedTypesDialog((t['id'] as int?) ?? (t['id_tipo_actividad'] as int? ?? 0), nombre),
             ),
             IconButton(
-              icon: const Icon(Icons.delete_rounded,
+              icon: const Icon(Icons.delete_outline,
                   color: Color(0xFFD9232A)),
               onPressed: () => _deleteType(id),
             ),
